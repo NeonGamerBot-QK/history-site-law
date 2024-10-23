1c0c94b6738ed9aa10b246b6ce9746ed7f756cb9
Saahil <neon@saahild.com>
2024-10-23 03:12:27 +0000

    fix: update content.

--- src/content.tsx
+++ src/content.tsx
@@ -1,23 +1,58 @@
 // my ac history work
 export const text = [
-  <>
+  <p>
     To get this started I would write an email to my local congressman where
-    they would get the bill drafted and the bill signed.
-  </>,
-  <>
-    After that it would go to a hopper and continue on for a committee hearing
-    which is usually in a subcommittee then to the markup process to be amended.
-    After the discussion the bill would be reported out to the full committee.
-    Once through the process of the full committee it may need review by the
-    Select Committee.
-  </>,
+    they would get the{" "}
+    <span className="tooltip" data-tip="approved by legislator.">
+      bill drafted
+    </span>{" "}
+    and the bill signed for not being allowed to watch talk tauh in delaware on
+    sundays.
+  </p>,
+  <p>
+    After that it would go to a hopper and continue on for a{" "}
+    <span
+      className="tooltip"
+      data-tip="committee gather to discuss about testamony"
+    >
+      committee hearing
+    </span>
+    which is usually in a{" "}
+    <span
+      className="tooltip"
+      data-tip=" specialized group within a larger legislative committee "
+    >
+      subcommittee
+    </span>{" "}
+    then to the markup process to be amended. After the discussion the bill
+    would be reported out to the full committee. Once through the process of the
+    full committee it may need review by the{" "}
+    <span className="tooltip" data-tip=" temporary legislative committee">
+      Select Committee
+    </span>
+    .
+  </p>,
   <p>
     Otherwise it will be passed to the rules committee. Which if approved by the
-    Speaker of the House would move it to floor debate
+    <span
+      className="tooltip"
+      data-tip="officer of the US HoR (House of Representatives)"
+    >
+      {" "}
+      Speaker of the House
+    </span>{" "}
+    would move it to floor debate
   </p>,
   <p>
     When this process is mirrored in the senate, where the Senate Majority
-    Leader would lead it instead of the Speaker of the house.
+    Leader would lead it instead of the{" "}
+    <span
+      className="tooltip"
+      data-tip="officer of the US HoR (House of Representatives)"
+    >
+      {" "}
+      Speaker of the House
+    </span>{" "}
   </p>,
   <p>
     Something that could happen is logrolling which could occur, where
@@ -34,6 +69,6 @@ export const text = [
   <p>
     If it were vetoed then the bill would have to start from the beginning if it
     is not overridden by congress. Then in the case my law doesnt get vetoed it
-    will be implemented and enforced.
+    will be implemented and enforced. then there would be no talk tauh
   </p>,
 ];
